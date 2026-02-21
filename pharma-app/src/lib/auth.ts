7c581123c99eb53f78830ba39c4a968efbf5a29c
Kenrique100 <ngwakenri2016@gmail.com>
2026-02-21 20:29:54 +0100

    updated files

--- pharma-app/src/lib/auth.ts
+++ pharma-app/src/lib/auth.ts
@@ -12,13 +12,6 @@ const users = [
     password: "$2b$10$2bhk5pvPw33W94YktS6jBuBTTi5.4JC/E8AAlJzmaSML9JYG7llGa", // admin123
     role: "admin",
   },
-  {
-    id: "2",
-    name: "Test User",
-    email: "user@pharmagrade.com",
-    password: "$2b$10$aKKJLWifABKYn8lNFE8GX.AGIpJmi2nVWAJRVWTmiUROY8MObyaM2", // user123
-    role: "user",
-  },
 ];
 
 const providers = [
